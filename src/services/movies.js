4730784aed97cef78ec0a1d94742e138397d81e8
rpayeras <payerasam@gmail.com>
2022-04-23 18:51:45 +0200

    Added env variables

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,9 +1,7 @@
-const API_KEY = '4287ad07'
-
 export const checkIsResponseOk = res => res.Response === 'True'
 
 export const searchMovies = ({ query }) => {
-  return window.fetch(`https://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`)
+  return window.fetch(`https://www.omdbapi.com/?s=${query}&i=${import.meta.env.VITE_API_ID}&apikey=${import.meta.env.VITE_API_KEY}`)
     .then(res => res.json())
     .then(response => {
       const resultsToStore = response.Response === 'True' ? response.Search : []
